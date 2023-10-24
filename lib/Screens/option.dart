23e5a53d9264947bf9924b57ca72b4b1b6d084e5
l224 <l224@cut.ac.za>
2023-10-24 16:18:21 +0200

    24 October updated

--- lib/Screens/option.dart
+++ lib/Screens/option.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -27,80 +28,129 @@ class _OptionsState extends State<Options> {
                 begin: Alignment.topRight,
                 end: Alignment.bottomCenter,
                 colors: [Colors.red, Colors.white])),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Center(
-                  child: Container(
-                    height: MediaQuery.of(context).size.height,
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            stops: const [0.1, 0.5],
-                            colors: [Colors.red.shade800, Colors.white60]),
-                        borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(100.0))),
-                    child: const Text(
-                      'Doctor',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
+        child: ListView(
+          children: [
+            CarouselSlider(
+                items: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.transparent,
+                        ),
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Doctor',
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: OutlinedButton(
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .pushNamed(RouteManager.doctorPage);
+                                },
+                                child: Text('Click here'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  width: 200,
-                  height: 100,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      color: Colors.red),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context)
-                          .pushNamed(RouteManager.pharmacistPage);
-                    },
-                    child: const Text(
-                      'Pharmacist',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.transparent,
+                        ),
+                        child: Column(
+                          children: [
+                            const Text('Pharmacist',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                )),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: OutlinedButton(
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .pushNamed(RouteManager.pharmacistPage);
+                                },
+                                child: Text('Click here'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  width: 200,
-                  height: 100,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      color: Colors.red),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context)
-                          .pushNamed(RouteManager.patientProfile);
-                    },
-                    child: const Text(
-                      'Patient',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.transparent,
+                        ),
+                        child: Column(
+                          children: [
+                            const Text('Patient',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                )),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: OutlinedButton(
+                                onPressed: () {
+                                  Navigator.of(context)
+                                      .pushNamed(RouteManager.patientProfile);
+                                },
+                                child: const Text('Click here'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-            ],
-          ),
+                ],
+                options: CarouselOptions(
+                    height: 300,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enableInfiniteScroll: true,
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 800),
+                    viewportFraction: 0.8))
+          ],
         ),
       ),
     );
