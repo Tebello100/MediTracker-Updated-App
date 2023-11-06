919b59b6083c26fe129bb4756e3f921683c4c369
Tebello100 <beketsanaabigail@gmail.com>
2023-11-06 03:32:26 +0200

    The integration work

--- build/app/generated/source/buildConfig/debug/com/example/helloworld/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/helloworld/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.helloworld;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.helloworld";
+  public static final String APPLICATION_ID = "Meditracker.mobile01";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
